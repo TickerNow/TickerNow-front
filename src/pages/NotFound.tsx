ae0878645c25a2a59b2cdb89166d22d9004b7895
dign1120 <dign1120@naver.com>
2025-06-01 01:30:51 +0900

    feat : add 404 Not Found 페이지

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+    return (
+        <div className="w-full h-screen flex flex-col items-center justify-center bg-gray-900 text-white">
+        <h1 className="text-6xl font-bold mb-4">404</h1>
+        <p className="text-xl">페이지를 찾을 수 없습니다.</p>
+        </div>
+    );
+}
